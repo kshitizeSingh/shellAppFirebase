30b6d5740d20301644f73b7d3182c370512c6ac9
Kshitize <kshitize@techprescient.com>
2021-02-07 23:35:25 +0530

    added bootstrap with login component

--- shellapp/src/app/components/login/login.component.ts
+++ shellapp/src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import {AuthService} from 'src/app/service/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(public auth: AuthService, public router:Router) { }
 
   ngOnInit(): void {
+    console.log('login')
   }
 
 }
--- shellapp/src/app/sharedModule/shared/shared.module.ts
+++ shellapp/src/app/sharedModule/shared/shared.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 //this module is used for styling libraries 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    MatCardModule,
+    MatIconModule,
+    MatGridListModule
+  ],
+  exports: [
+    MatCardModule,
+    MatIconModule,
+    MatGridListModule
   ]
 })
 export class SharedModule { }
